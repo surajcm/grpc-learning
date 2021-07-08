7a5c1320fa88b262a7f5f864750cee866e5503e2
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-07-08 09:25:10 +0530

    first commit

---
+++ src/main/java/com/suraj/grpclearning/GrpcLearningApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.grpclearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suraj/grpclearning/GrpcLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.grpclearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
